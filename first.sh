26a98f516e9271db0d0c6c8ac1ea2bf8a46af583
Your Name <you@example.com>
2019-07-05 21:45:48 +0200

    shshshhs

--- first.sh
+++ first.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "WARNING....Are you shure you want delete /...."
+echo "WARNING....Are you shure you want delete </>....?"
 sleep 3
-echo "YES"
+echo "YES!!!"
 sleep 2
-echo "I am so sory....By-by"
+echo "Okay....By-by"
 sleep 1
 
 j=5
@@ -13,6 +13,14 @@ for ((i=1;i<=5;i++));
 do
       echo $j
       j=$[$j-1]
-      sleep 1
+      sleep 0.5
 done
 echo "Privet ot Maksa"
+echo FATAL!!!
+echo FATAL!!!
+echo FATAL!!!
+echo FATAL!!!
+echo KAPUT!!!
+echo PIZDEC!!!
+echo YA EBU!!!
+echo NAH*YA???
